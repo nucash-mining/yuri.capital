f19aa14ab0ae8b2e70f66e229017dc2f40e3b912
NuCash <39842364+nucash-mining@users.noreply.github.com>
2024-01-10 23:15:37 -0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"659f77b1867c86001f1a6b22",
+				"659f77b1867c86001f1a6b25"
+			]
+		},
+		"659f77b1867c86001f1a6b22": {
+			"id": "659f77b1867c86001f1a6b22",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"659f77b1867c86001f1a6b25": {
+			"id": "659f77b1867c86001f1a6b25",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Strong, Button, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Text margin="0px 0px 0px 0px" />
+		<Box min-width="100px" min-height="100px" position="static">
+			<Button>
+				<Strong>
+					Connect Wallet
+				</Strong>
+			</Button>
+		</Box>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"659f77b1867c86001f1a6b1b"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
